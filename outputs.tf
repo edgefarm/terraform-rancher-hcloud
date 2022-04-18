8c3001c6a1ba992405a53b81ff8170fdec96ab06
Florian Reinhold <florian.reinhold@ci4rail.com>
2022-04-18 21:18:08 +0200

    refactor: move all outputs to output.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "rancher_url" {
   description = "url via which rancher can be accessed."
   value       = local.rancher_hosturl
 }
+
+output "rancher_admin_token" {
+  description = "rancher admin token to modify rancher."
+  value       = rancher2_bootstrap.setup_admin.token
+}
+
+output "hetzner_driver_id" {
+  description = "hetzner node driver id."
+  value       = rancher2_node_driver.hetzner_node_driver.id
+}
